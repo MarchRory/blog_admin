bda29fe3c91fe993626cde46cc45201082173842
LiuShi_2 <2287722838@qq.com>
2023-11-20 00:29:16 +0800

    feat: 文章、标签、友链、个人信息的crud

---
+++ .history/.prettierrc_20231120000353.js
@@ -0,0 +1,23 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+<<<<<<< HEAD:.prettierrc.js
+  plugins: ['prettier-plugin-packagejson'],
+  overrides: [
+    {
+      files: '.*rc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
+=======
+}
+>>>>>>> thin:prettier.config.js
